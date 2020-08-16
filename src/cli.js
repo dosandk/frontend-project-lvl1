6337637cf9e6f63f71ef0ff209a887cc069db553
Volodymyr Shevchuk <dosandkv@gmail.com>
2020-08-16 22:21:08 +0300

    step #4 in progress;

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default function () {
+export default function askName() {
   console.log('Welcome to the Brain Games!');
 
   const name = readlineSync.question('May I have your name? ');
